ae60320d722e5fc40ae454b47cb71781b42c403a
gcrespi <crespi.gustavo@gmail.com>
2016-09-08 09:19:24 -0300

    Fixing Typo on migration

--- tusLibros/db/migrate/20160906143331_add_association_many_to_many_between_carts_and_books.rb
+++ tusLibros/db/migrate/20160906143331_add_association_many_to_many_between_carts_and_books.rb
@@ -2,7 +2,7 @@ class AddAssociationManyToManyBetweenCartsAndBooks < ActiveRecord::Migration
   def change
     create_table :books_carts, id: false do |t|
       t.belongs_to :book, index: true
-      t.belongs_to :carts, index: true
+      t.belongs_to :cart, index: true
     end
   end
 end
