cee8a8e49e3a7c32c03896a082a34dc4a134cb7f
gcrespi <crespi.gustavo@gmail.com>
2016-09-20 13:56:36 -0300

    Configuring CORS

--- tusLibros/app/controllers/application_controller.rb
+++ tusLibros/app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   include WithErrorHandler
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   around_action :handle_domain_errors
 end
