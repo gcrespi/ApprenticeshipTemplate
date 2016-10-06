c78b6e7b528c2b78a9db879d86e738d370602f4a
gcrespi <crespi.gustavo@gmail.com>
2016-10-06 13:20:34 -0300

    Beginning with ticTacToe IU

---
+++ ticTacToeUI/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ticTacToeUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ticTacToeUiApp
+ */
+angular.module('ticTacToeUiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ ticTacToeUI/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ticTacToeUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ticTacToeUiApp
+ */
+angular.module('ticTacToeUiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
