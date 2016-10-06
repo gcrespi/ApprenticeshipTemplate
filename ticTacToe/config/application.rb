307ed67891710ffbb46d7d21e485aa45dfdabd5d
gcrespi <crespi.gustavo@gmail.com>
2016-10-06 13:18:33 -0300

    Adding cors tu tictactoe Backend

--- ticTacToe/config/application.rb
+++ ticTacToe/config/application.rb
@@ -31,6 +31,12 @@ module TicTacToe
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
 
     config.autoload_paths += %W(#{config.root}/app/models/board_state)
     config.autoload_paths += %W(#{config.root}/app/models/winner_rule)
